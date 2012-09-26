a2defa55e0e92f2ac3ff1e683360a6b8327ee47b
unknown <jgillham@MSCD107D.academia.metro>
2012-09-26 15:45:52 -0600

    Fixed compiler errors.

--- HW3/parse.s
+++ HW3/parse.s
@@ -1,27 +1,28 @@
 	AREA parse, CODE
 SWI_WriteC	EQU 	&0	; Output character in r0
 SWI_Exit	EQU	&11	; Exit the program
+
 	ENTRY
 start
-	ADR	r5, STR1	; Copy in pointer to string
+	ADR	r1, STR1	; Copy in pointer to string
 	BL	print_string	; Call print string
 
-	MOV	r10, STR1	; r1= r0
+	ADR	r2, STR1	; r2= address of STR1
 parse1
-	LDRB	r1, [r2], #1	; r1= *r2; ++r2
+	LDRB	r1, [r2], #1	; byte r1= *r2; ++r2
 	CMP	r1, #0		; Check for null ; Make previous line copy byte from adr
-	BNE	parse_done	; While (char !null)
+	BEQ	parse_done	; While (char !null)
 	BL	check_vowel	; Call check_vowel
 	B	parse1		; Loop back
 
 parse_done
-	ADR	r5, INSTR	; Load pointer for argument
+	ADR	r1, STR1	; Load pointer for argument
 	BL	print_string	; Print result
 	SWI	SWI_Exit	; Exit the program
 	
 
 print_string
-	LDRB 	r0, [r5], #1	; r0= *r5; ++r5; Store the value from the pointer r5 then increment r5
+	LDRB 	r0, [r1], #1	; r0= *r5; ++r5; Store the value from the pointer r5 then increment r5
 	CMP	r0, #0		; Check for null
 	MOVEQ	pc, r14		; If null, Return ; r14 is special
 	SWI	SWI_WriteC	; Print signal character in r0
@@ -34,8 +35,9 @@ check_vowel
 	TEQNE	r1, #'i'	; r1 == 'i'
 	TEQNE	r1, #'u'	; r1 == 'u'
 	TEQNE	r1, #'y'	; r1 == 'y'
-	ADDEQ	r1, r1, #20	; Make uppercase
+	ADDEQ	r2, r2, #20	; Make uppercase
 	ADDEQ	r7, r7, #1	; Increment vowel counter
+
 	TEQ	r1, #'A'	; r1 == 'A'
 	TEQNE	r1, #'E'	; r1 == 'E'
 	TEQNE	r1, #'I'	; r1 == 'I'
@@ -43,10 +45,11 @@ check_vowel
 	TEQNE	r1, #'U'	; r1 == 'U'
 	TEQNE	r1, #'Y'	; r1 == 'Y'
 	ADDEQ	r7, r7, #1	; Increment vowel counter
+
 	MOV	pc, r14		; Return
 
-	END
 
 STR1	DCB	"This is a soft test.", 0
 ;  OUTSTR	=	"                    ", 0
-ALIGN
+	ALIGN
+	END
