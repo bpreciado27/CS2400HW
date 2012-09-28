e6848095fb82340e90d2fe163a9583cbf80ecb1f
Josh Gillham <usajoshgillham@gmail.com>
2012-09-28 14:00:16 -0600

    Added a function hex_encode.

--- HW3/parse.s
+++ HW3/parse.s
@@ -17,8 +17,9 @@ parse1
 	B	parse1		; Loop back
 
 parse_done
-	ADR	r1, STR1	; Load pointer for argument
+	ADR	r1, STR1	; void* r1= &STR1; Load pointer for argument
 	BL	print_string	; Print result
+	BL	hex_encode	; Print count of the vowels in hex code
 
 	SWI	SWI_Exit	; Exit the program
 	
@@ -56,8 +57,21 @@ check_vowel
 
 	MOV	pc, r14		; Return
 
-
+; @arg r7
+hex_encode
+	ADR	r0, LOWNIB	; void* r0= &LOWNIB; Get the address of LOWNIB
+	LDR	r0, [r0]	; int r0= *r0; Get the value from the address in r0
+	AND	r5, r7, r0	; char r5= r7 & *r0; Get the low nibble
+	ADR	r0, HEXCODE	; void* r0= &HEXCODE; Get the address of HEXCODE
+	ADD	r0, r6, r0	; char r0= r0[ r5 ]; Treat the address of HEXCODE like an array
+	SWI	SWI_WriteC	; Print signal character in r0
+	MOV	pc, r14		; Return
+ 
+LOWNIB	DCD	&00,&0F
 STR1	DCB	"This is a soft test.", 0
+HEXCODE DCB	"0123456789ABCDEF", 0
+
+
 ;  OUTSTR	=	"                    ", 0
 	ALIGN
 	END
